26b56f56a9e446e9d2ac45226a1ded6c685f3308
Ralph Marschall <marschallralph@me.com>
2017-05-28 15:37:25 +0200

    Implement basic dev/prod config

---
+++ config/config.py
@@ -0,0 +1,9 @@
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    
+    TEMENOS_HOST = '127.0.0.1'
+    TEMENOS_USER = 'user'
+    TEMENOS_PASSWORD  = 'password'
+    
--- server.py
+++ server.py
@@ -4,12 +4,32 @@ from modules.account.account import account_api
 
 app = Flask(__name__)
 
+# Configure Application
+###############################################################################
+
+app.config.from_object('config.dev.config-dev.Development')
+
+# Register Routes
+###############################################################################
+
 app.register_blueprint(account_api)
 
 @app.route("/")
 def welcome():
     return "Welcome to the Python Server to use Temenos!"
 
+# Main Entry Point
+###############################################################################
+
 if __name__ == "__main__":
     port = 8080
+    
+    print('Launching Server on port {}...'.format(port))
+    
+    print('\n*** CONFIG ***\n')
+    print('Temenos Config:')
+    print('  - HOST : {}'.format( app.config['TEMENOS_HOST'] ))
+    print('  - USER : {}'.format( app.config['TEMENOS_USER'] ))
+    print()
+    
     app.run(port = port)
