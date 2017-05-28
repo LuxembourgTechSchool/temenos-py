da95182f278ed90280159985113a5b244d8b7954
Anush Manukyan <manukyananush88@gmail.com>
2017-05-28 16:53:53 +0200

    Implement get_accounts and get_account api

--- modules/account/account.py
+++ modules/account/account.py
@@ -1,8 +1,22 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import current_app as app
+
+from modules.models.temenos import Temenos
+
+import requests
+
 
 account_api = Blueprint('account_api', __name__)
 
 @account_api.route('/accounts', methods=['GET'])
 def get_accounts():
-    return jsonify([])
+    temenos = Temenos()
+    r = temenos.get_accounts()
+    return jsonify(r.json())
+
+@account_api.route('/accounts/<account_id>', methods=['GET'])
+def get_account(account_id):
+    temenos = Temenos()
+    r = temenos.get_account(account_id)
+    return jsonify(r.json())
