9106ddec53ccfd2a4e8258310e3c953e2b39278f
Anush Manukyan <manukyananush88@gmail.com>
2017-05-28 18:25:17 +0200

    Implement processing of Temenos response

--- modules/account/account.py
+++ modules/account/account.py
@@ -11,12 +11,32 @@ account_api = Blueprint('account_api', __name__)
 
 @account_api.route('/accounts', methods=['GET'])
 def get_accounts():
-    temenos = Temenos()
-    r = temenos.get_accounts()
-    return jsonify(r.json())
+    r = Temenos().get_accounts()
+
+    items = r.json()['_embedded']['item']
+    data = []
+
+    for account in items:
+        simple_account = { 
+            'id': account['AccountNumber'],
+            'customer_id': account['Customer'],
+            'currency': account['Currency']
+        }
+
+        data.append( simple_account )
+
+    return jsonify(data)
 
 @account_api.route('/accounts/<account_id>', methods=['GET'])
 def get_account(account_id):
-    temenos = Temenos()
-    r = temenos.get_account(account_id)
-    return jsonify(r.json())
+    r = Temenos().get_account(account_id)
+    account = r.json()
+    account_details = {
+        'id': account['Acc'],
+        'account_name': account['Ac1'],
+        'customer_id': account['Customer'],
+        'balance': account['RealBalance'],
+        'bank': account['Branch']
+    }
+
+    return jsonify(account_details)
