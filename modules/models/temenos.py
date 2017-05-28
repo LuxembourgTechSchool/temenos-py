e7ece30d113cdb4822ec655172f6adeedd425e37
Ralph Marschall <marschallralph@me.com>
2017-05-28 16:44:55 +0200

    Implement start of class to make Temenors requests

---
+++ modules/models/temenos.py
@@ -0,0 +1,29 @@
+from flask import current_app as app
+
+import requests
+
+class Temenos():
+    
+    def __init__(self):
+        self.auth = ( app.config['TEMENOS_USER'], app.config['TEMENOS_PASSWORD'] )
+        self.host = app.config['TEMENOS_HOST']
+        
+    def get_accounts(self):
+        pass
+    
+    def get_account(self, account_id):
+        pass
+    
+    def __prepare_session(self):
+        s = requests.Session()
+        s.auth = self.auth
+        s.headers.update({'Accept': 'application/hal+json' , 'Content-Type': 'application/hal+json'})
+        
+        return s
+    
+    def _do_get_request(self, api):
+        s = self.__prepare_session()
+        
+        r = s.get('{}/TCMBCommon-iris/TCMBCommon.svc/GB0010001/{}'.format( self.host, api ) )
+        
+        return r
